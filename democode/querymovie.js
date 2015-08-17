bf65258c1cbd6a6dec63e18caf45ae622fdf81dd
Ryan CrawCour <ryancrawcour@outlook.com>
2015-08-17 09:13:56 -0700

    fixed code to use config, and just work

--- democode/querymovie.js
+++ democode/querymovie.js
@@ -6,24 +6,24 @@ var DocumentDBClient = require('documentdb').DocumentClient
   , config = require('./config')
   , databaseId = config.names.database
   , collectionId = config.names.collection
-  , host = [];
-  , masterKey = [];
+  , host = config.connection.endpoint
+  , masterKey = config.connection.authKey
 
 
 if (process.argv.length <= 2) {
     console.log("Usage: querymovie <movietitle>");
     process.exit(-1);
 }
- 
-var movietitle = process.argv[2];
+
+var movieTitle = process.argv[2];
 
 var client = new DocumentDBClient(host, {masterKey: masterKey});
 
-var collLink = "dbs/" + databaseId+ "/colls/"+collectionId;
+var collLink = 'dbs/' + databaseId+ '/colls/'+ collectionId;
 
-var query = "SELECT * from Movies m WHERE m.title = '" + movietitle + "'";
+var query = 'SELECT * from Movies m WHERE m.title = \'' + movieTitle + '\'';
 
-client.queryDocumentscollLink, query).toArray(function (err, results) {
+client.queryDocuments(collLink, query).toArray(function (err, results) {
             if (err) {
                 console.log(err);
 
